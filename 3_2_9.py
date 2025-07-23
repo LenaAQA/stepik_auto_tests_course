00c099e3deee865a35370e5ff42a759fff557c59
LenaQA94 <lenaqa94@gmail.com>
2025-07-23 14:22:59 +0300

    Update 3_2_9.py

--- 3_2_9.py
+++ 3_2_9.py
@@ -1,3 +1,12 @@
 def test_substring(full_string, substring):
     assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
 
+r = 0
+res = []
+for i in range(77):
+    p = r * 10 + 7
+    res.append(p)
+    r = p
+my_frozen = frozenset(res)
+print(res)
+print(my_frozen)
