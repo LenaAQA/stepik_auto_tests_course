f4e0be55a0f2d99acce2f7afda1c91e9a8425352
LenaQA94 <lenaqa94@gmail.com>
2025-07-23 14:23:09 +0300

    Update 3_2_9.py

--- 3_2_9.py
+++ 3_2_9.py
@@ -1,6 +1,5 @@
 def test_substring(full_string, substring):
     assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
-
 r = 0
 res = []
 for i in range(77):
