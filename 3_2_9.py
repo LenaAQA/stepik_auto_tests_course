77c73ae63e04b99133ef46296ed9834646209b11
LenaQA94 <lenaqa94@gmail.com>
2025-07-23 14:23:19 +0300

    Update 3_2_9.py

--- 3_2_9.py
+++ 3_2_9.py
@@ -9,3 +9,12 @@ for i in range(77):
 my_frozen = frozenset(res)
 print(res)
 print(my_frozen)
+r = 0
+res = []
+for i in range(77):
+    p = r * 10 + 7
+    res.append(p)
+    r = p
+my_frozen = frozenset(res)
+print(res)
+print(my_frozen)
